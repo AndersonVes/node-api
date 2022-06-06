636492ae85da84b9aee020269d803f6a814d6130
Anderson Ves <avesguerber@gmail.com>
2022-06-06 16:40:25 -0300

    observation

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -5,3 +5,5 @@ mongoose.connect("mongodb+srv://anderson:123@cluster0.xxcpq.mongodb.net/node-db"
 let db = mongoose.connection;
 
 export default db;
+
+//this is a training project, there's no private information on the database
